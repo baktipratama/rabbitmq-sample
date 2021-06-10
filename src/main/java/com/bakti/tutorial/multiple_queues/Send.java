833d1a00756d20ea04ac4c76933d925a8e3e31a7
bakti.yudha <bakti.yudha@go-jek.com>
2021-06-10 11:47:08 +0700

    create example for multiple queues

---
+++ src/main/java/com/bakti/tutorial/multiple_queues/Send.java
@@ -0,0 +1,31 @@
+package com.bakti.tutorial.multiple_queues;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.nio.charset.StandardCharsets;
+
+
+public class Send {
+    private final static String EXCHANGE_NAME = "logs";
+    private final static String QUEUE1 = "queue1";
+    private final static String QUEUE2 = "queue2";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try (Connection connection = factory.newConnection();
+            Channel channel = connection.createChannel()) {
+            channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
+            channel.queueBind(QUEUE1, EXCHANGE_NAME, "");
+            channel.queueBind(QUEUE2, EXCHANGE_NAME, "");
+
+            String message = "Hello Bakti!";
+            for(int i=0;i<10;i++){
+                channel.basicPublish(EXCHANGE_NAME, "", null, message.getBytes(StandardCharsets.UTF_8));
+                System.out.println(" [x] Sent '" + message + "'");
+            }
+        }
+    }
+}
